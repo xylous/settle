684294c2ad0a485a6db253a064f575487007e0a9
xylous <xylous.e@gmail.com>
2023-01-24 21:32:03 +0200

    Fix: graph: don't repeat notes

--- src/graph.rs
+++ src/graph.rs
@@ -1,20 +1,47 @@
 use crate::Zettel;
 use petgraph::dot::{Config, Dot};
+use petgraph::graph::NodeIndex;
 use petgraph::Graph;
 
 /// Turn a Zettelkasten into a directed graph, using petgraph
 fn gen_graph(zs: &[Zettel]) -> Graph<&str, &str>
 {
     let mut graph = Graph::<&str, &str>::new();
-    //let mut links: Vec<(&str, &str)> = vec![];
+    let mut idxs = vec![];
     for z in zs {
-        let t_node = graph.add_node(&z.title);
+        // basically, figure out if we've seen the current Zettel, that is, if it's already
+        // added to the graph, because every entry should be added only once
+        let (_, seen_idx) = find_seen_by_name(idxs.clone(), &z.title);
+        let t_idx = if seen_idx == NodeIndex::default() {
+            let idx = graph.add_node(&z.title);
+            idxs.push((&z.title, idx));
+            idx
+        } else {
+            seen_idx
+        };
+
+        // the same is also checked for with every link
         for l in &z.links {
-            let l_node = graph.add_node(&l);
-            graph.extend_with_edges(&[(t_node, l_node)]);
+            let (_, seen_idx) = find_seen_by_name(idxs.clone(), l);
+            let l_idx = if seen_idx == NodeIndex::default() {
+                let idx = graph.add_node(l);
+                idxs.push((l, idx));
+                idx
+            } else {
+                seen_idx
+            };
+            graph.extend_with_edges(&[(t_idx, l_idx)]);
         }
     }
     graph
+}
+
+fn find_seen_by_name(idxs: Vec<(&str, NodeIndex)>, name: &str) -> (String, NodeIndex)
+{
+    let (n, k) = idxs.into_iter()
+                     .find(|(v, _)| v == &name)
+                     .unwrap_or_default();
+    (n.to_string(), k)
 }
 
 /// Turn a graph into its dot format, printing it to stdout
